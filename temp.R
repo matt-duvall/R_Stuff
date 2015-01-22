3034b9d485f47220d5c8eb1ab4a00c5abed1c039
matt-duvall <mduvall@bates.edu>
2015-01-22 13:45:08 -0500

    first commit

---
+++ temp.R
@@ -0,0 +1,10 @@
+View(DCS_20141124)
+table(DCS_20141124$have_major)
+312/334
+1950/334
+334/1950
+hist(DCS_20141124$have_major, plot=TRUE)
+43/334
+table(DCS_20141124$courses)
+219/334
+map(database="state", region="maine")
